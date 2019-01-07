9f11f9de2a0ff6243b1ac4a5c7936f57bafa5484
Raphael Pinel <raphaelpinel8@gmail.com>
2019-01-07 11:41:15 +0200

    introduced router for checkout and passed data to orderSummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,6 +29,8 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
+
     axios
       .get('https://deli-burger.firebaseio.com/ingredients.json')
       .then(response => {
@@ -95,28 +97,41 @@ class BurgerBuilder extends Component {
   };
   purchaseContinueHandler = () => {
     // alert("let's continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice, // in a real App, should recalculate the price on the server
-      customer: {
-        name: 'Matti Heikkinen',
-        address: {
-          street: 'Hämeenkatu 111',
-          zipCode: '33180',
-          city: 'Tampere',
-          country: 'Finland'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => this.setState({ loading: false, purchasing: false }));
+    //   this.setState({ loading: true });
+    //   const order = {
+    //     ingredients: this.state.ingredients,
+    //     price: this.state.totalPrice, // in a real App, should recalculate the price on the server
+    //     customer: {
+    //       name: 'Matti Heikkinen',
+    //       address: {
+    //         street: 'Hämeenkatu 111',
+    //         zipCode: '33180',
+    //         city: 'Tampere',
+    //         country: 'Finland'
+    //       },
+    //       email: 'test@test.com'
+    //     },
+    //     deliveryMethod: 'fastest'
+    //   };
+    //   axios
+    //     .post('/orders.json', order)
+    //     .then(response => {
+    //       this.setState({ loading: false, purchasing: false });
+    //     })
+    //     .catch(error => this.setState({ loading: false, purchasing: false }));
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   };
 
   render() {
